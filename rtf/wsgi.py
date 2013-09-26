86780bdbfa7c7ce4a585c12830551fe46a4c156a
Bob Waycott <bobwaycott@gmail.com>
2013-09-26 03:04:21 -0400

    grr

--- rtf/wsgi.py
+++ rtf/wsgi.py
@@ -1,9 +1,10 @@
 from os import sep
 from os import environ
 from os.path import abspath, dirname
-from django.core.wsgi import get_wsgi_application
 
 PROJECT_DIR = dirname(abspath(__file__))
 settings_module = "%s.settings" % PROJECT_DIR.split(sep)[-1]
 environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
