cebbc1736e7e649165a29fd863b18b23674f5ff5
Bob Waycott <bobwaycott@gmail.com>
2013-09-26 03:01:09 -0400

    wsgi cleanup

--- rtf/wsgi.py
+++ rtf/wsgi.py
@@ -1,9 +1,9 @@
 from os import sep
+from os.environ import setdefault
 from os.path import abspath, dirname
-
-PROJECT_ROOT = dirname(abspath(__file__))
-settings_module = "%s.settings" % PROJECT_ROOT.split(sep)[-1]
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
-
 from django.core.wsgi import get_wsgi_application
+
+PROJECT_DIR = dirname(abspath(__file__))
+settings_module = "%s.settings" % PROJECT_DIR.split(sep)[-1]
+setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 application = get_wsgi_application()
