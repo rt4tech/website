c67cbae5fec3a61cb8c8658410dd8a173524c224
Bob Waycott <bobwaycott@gmail.com>
2013-09-26 02:55:04 -0400

    update wsgi

--- rtf/wsgi.py
+++ rtf/wsgi.py
@@ -1,8 +1,8 @@
+from os import sep
+from os.path import abspath, dirname
 
-import os
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
+PROJECT_ROOT = dirname(abspath(__file__))
+settings_module = "%s.settings" % PROJECT_ROOT.split(sep)[-1]
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 from django.core.wsgi import get_wsgi_application
