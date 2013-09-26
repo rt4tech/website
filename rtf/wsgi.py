250c3ac5891854d0a8e89343e2d5047469697843
Bob Waycott <bobwaycott@gmail.com>
2013-09-26 03:03:38 -0400

    that's a variable, not a module

--- rtf/wsgi.py
+++ rtf/wsgi.py
@@ -1,9 +1,9 @@
 from os import sep
-from os.environ import setdefault
+from os import environ
 from os.path import abspath, dirname
 from django.core.wsgi import get_wsgi_application
 
 PROJECT_DIR = dirname(abspath(__file__))
 settings_module = "%s.settings" % PROJECT_DIR.split(sep)[-1]
-setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 application = get_wsgi_application()
